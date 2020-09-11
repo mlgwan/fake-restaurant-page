bbc8c3b892ab6b22cc2d71629d64001c02aa7311
Ciwan Clifford <theciwan@gmail.com>
2020-09-11 07:33:36 +0200

    ForgForgot to create a repository earlier again. So far the main page's content is loaded through a module, next up is the nav bar and the four modules for its content.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import createInitialPage from "./initialPage";
+
+
+createInitialPage();
---
+++ src/initialPage.js
@@ -0,0 +1,20 @@
+function createInitialPage (){
+    let content = document.getElementById("content");
+
+    let upperContent = document.createElement("div");
+    upperContent.id = "upper-content";
+    let heading = document.createElement("h1");
+    heading.innerText = "Nice Breakfast Place";
+    upperContent.appendChild(heading);
+
+    let lowerContent = document.createElement("div");
+    lowerContent.id = "lower-content";
+    let descriptionText = document.createElement("span");
+    descriptionText.innerText = "Expand your culinary horizons in a welcoming and relaxing environment. Our Michelin-starred chef Dante Jacuzzi will serve you some of the most innovative dishes your eyes have ever gazed upon and will ensure to leave them as satisfied as your appetite. Feast on a conglomerate of carefully selected meals from around the world.";
+    lowerContent.appendChild(descriptionText);
+
+    content.appendChild(upperContent);
+    content.appendChild(lowerContent);
+}
+
+export default createInitialPage;
